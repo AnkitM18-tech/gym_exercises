624cc78fd95b9f3f7a1f099a58069b5605545ac4
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2023-01-28 01:47:28 +0530

    exerciseVideos

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  if (!exerciseVideos.length) return "Loading...";
+  return (
+    <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }} p="20px">
+      <Typography variant="h3" mb="33px">
+        Watch{" "}
+        <span style={{ color: "#ff2625", textTransform: "capitalize" }}>
+          {name}
+        </span>{" "}
+        Exercise Videos
+      </Typography>
+      <Stack
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+        sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0" } }}
+      >
+        {exerciseVideos?.slice(0, 6).map((video, index) => (
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${video.video.videoId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={video.video.thumbnails[0].url} alt={video.video.title} />
+            <Box>
+              <Typography variant="h5" color="#000">
+                {video.video.title}
+              </Typography>
+              <Typography variant="h7" color="#000">
+                {video.video.channelName}
+              </Typography>
+            </Box>
+          </a>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
+
+export default ExerciseVideos;
